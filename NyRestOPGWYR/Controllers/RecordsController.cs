a710a73eafbafbcba3757834726cf7ed117066c1
WameG <wgassama@yahoo.dk>
2023-03-21 11:24:36 +0100

    Finish with the controller

---
+++ NyRestOPGWYR/Controllers/RecordsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NyRestOPGWYR.Models;
+using NyRestOPGWYR.Repository;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NyRestOPGWYR.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecordsController : ControllerBase
+    {
+        private RecordRepository _recordRepository;
+
+        public RecordsController(RecordRepository recordRepository)
+        {
+            _recordRepository = recordRepository;
+        }
+
+        // GET: api/<RecordController>
+        [HttpGet]
+        public IEnumerable<Record> Get([FromQuery] string title, [FromQuery] string sortBy)
+        {
+            return _recordRepository.GetAll(title, sortBy);
+        }
+
+        // GET api/<RecordController>/5
+        [HttpGet("{id}")]
+        public Record? Get(int id)
+        {
+            return _recordRepository.GetById(id);
+        }
+
+        // POST api/<RecordController>
+        [HttpPost]
+        public Record Post([FromBody] Record value)
+        {
+            return _recordRepository.Add(value);
+        }
+
+        // PUT api/<RecordController>/5
+        [HttpPut("{id}")]
+        public Record? Put(int id, [FromBody] Record value)
+        {
+            return _recordRepository.Update(id, value);
+        }
+
+        // DELETE api/<RecordController>/5
+        [HttpDelete("{id}")]
+        public Record? Delete(int id)
+        {
+            return _recordRepository.Delete(id);
+        }
+    }
+}
--- NyRestOPGWYR/Repository/RecordRepository.cs
+++ NyRestOPGWYR/Repository/RecordRepository.cs
@@ -1,5 +1,7 @@
 ﻿using NyRestOPGWYR.Models;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace NyRestOPGWYR.Repository
 {
@@ -18,6 +20,55 @@ namespace NyRestOPGWYR.Repository
                 new Record() { Id = _nextId++, Artist = "Renas", Duration = 3, PublicationYear = 2023, Title = "Hey Jones" },
                 new Record() { Id = _nextId++, Artist = "Gargamel", Duration = 5, PublicationYear = 2021, Title = "Hey smølf" },
             };
+        }
+
+        public List<Record> GetAll(string title, string sortBy)
+        {
+            List<Record> musics = new List<Record>(_records);
+
+            if (title != null)
+            {
+                musics = musics.FindAll(musics => musics.Title != null && musics.Title.StartsWith(title));
+            }
+            if (sortBy != null)
+            {
+                switch (sortBy.ToLower())
+                {
+                    case "title":
+                        musics = musics.OrderBy(musics => musics.Title).ToList();
+                        break;
+                }
+            }
+            return musics;
+        }
+
+        public Record? GetById(int id)
+        {
+            return _records.Find(musics => musics.Id == id);
+        }
+
+        public Record Add(Record newMusic)
+        {
+            newMusic.Id = _nextId++;
+            _records.Add(newMusic);
+            return newMusic;
+        }
+
+        public Record? Delete(int id)
+        {
+            Record? music = _records.Find(music1 => music1.Id == id);
+            if (music == null) return null;
+            _records.Remove(music);
+            return music;
+        }
+
+        public Record? Update(int id, Record updates)
+        {
+            Record? music = _records.Find(music1 => music1.Id == id);
+            if (music == null) return null;
+            music.Title = updates.Title;
+            music.Duration = updates.Duration;
+            return music;
         }
     }
 }
